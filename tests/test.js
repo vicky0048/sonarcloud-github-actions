d33798f8081fdd65d608557f4c1430d501c414c7
Sonu Sathyadas <githubtraining@wibmo.com>
2022-10-07 23:45:19 +0530

    ci-based-analysis-added

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,6 @@ test('adding two numbers', () => {
     expect(calculator.sum(3,4)).toBe(7);
   });
 
-  test('subtracting two numbers', () => {
+test('subtracting two numbers', () => {
     expect(calculator.subtract(3,4)).toBe(-1);
-  });
+});
